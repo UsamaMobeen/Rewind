42838dd1e0cd3ba18b1f6a72a5f300e7d78207fa
UsamaMobeen <Baadshaah18@gmail.com>
2021-07-26 22:23:47 +0500

    Build Complete Project
    Javascript Rewind project. design using BS, JS logic and code itself.

--- js/site.js
+++ js/site.js
@@ -1,4 +1,39 @@
-//Call Hello  World
-function HelloWorld(){
-    alert("Hello World");
+//Get the string from the Page
+//Controller function
+function getString(){
+
+   document.getElementById("alert").classList.add("invisible");
+    
+   let userString = document.getElementById("userString").value;
+   
+   let revString = reverseString(userString);
+
+   //alert("Reversed String is:" + revString);
+    displayString(revString);
+}
+
+// Reverse the string
+//Logic Function
+function reverseString(userSting){
+
+    let reverseString = [];
+
+    //
+
+    for (let index = userSting.length - 1; index >= 0; index--) {
+        reverseString += userSting[index];
+    }
+
+    return reverseString;
+}
+
+//Display the reversed string to user
+//View function
+function displayString(revString){
+
+    //write msg to page
+    document.getElementById("msg").innerHTML = `Your reversed string: ${revString}`;
+    //show alert box
+    document.getElementById("alert").classList.remove("invisible");
+
 }
